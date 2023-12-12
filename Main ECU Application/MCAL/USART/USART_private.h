dd8bfad54278ab8bfe0cec10f8c6812147aad019
unknown <ahmedm.elshamy23@gmail.com>
2023-12-12 20:27:16 +0200

    modified UART base address

--- Main ECU Application/MCAL/USART/USART_private.h
+++ Main ECU Application/MCAL/USART/USART_private.h
@@ -1,10 +1,10 @@
 #ifndef USART_PRIVATE_H
 #define USART_PRIVATE_H
 
-#define USART_SR *((volatile u32 *)(0x40004800))
-#define USART_DR *((volatile u32 *)(0x40004804))
-#define USART_BRR *((volatile u32 *)(0x40004808))
-#define USART_CR1 *((volatile u32 *)(0x4000480C))
+#define USART_SR *((volatile u32 *)(0x40003800))
+#define USART_DR *((volatile u32 *)(0x40003804))
+#define USART_BRR *((volatile u32 *)(0x40003808))
+#define USART_CR1 *((volatile u32 *)(0x4000380C))
 
 #define USART_CR1_MASK ((u32)0b10010000100000)
 #define USART_BAUD_9600_MASK ((u32)0b111010100110)
